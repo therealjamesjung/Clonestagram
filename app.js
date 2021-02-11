0e94b1ab5d1e54d532a3198775cc09dfe38ee82e
jamesj0918 <jamesj0107@naver.com>
2021-02-11 12:24:18 +0900

    Add API root for user, post api

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ const bodyparser = require("body-parser");
 
 const app = express();
 const port = 3000;
+const API_ROOT = "/api/v1/";
+
+const user = require("./api/user/user");
+const post = require("./api/post/post");
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
+
+app.use(API_ROOT, user);
+app.use(API_ROOT, post);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
