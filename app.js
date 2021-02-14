28cb09a53ff209b24d22e70dce37f7d2e3d2bb2a
miseongk <39994337+miseongk@users.noreply.github.com>
2021-02-14 17:21:50 +0900

    Add API root for comment api

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ const API_ROOT = "/api/v1/";
 
 const user = require("./api/user/user");
 const post = require("./api/post/post");
+const comment = require("./api/comment/comment");
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 
 app.use(API_ROOT, user);
 app.use(API_ROOT, post);
+app.use(API_ROOT, comment);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
