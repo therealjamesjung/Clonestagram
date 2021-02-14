191893b5abbf8d75f303ffefd202d0eddba31a3f
daye <leeda1801@gmail.com>
2021-02-14 13:11:32 +0900

    Add basic CRUD operation

--- utils/utils.js
+++ utils/utils.js
@@ -11,4 +11,50 @@ const _validate_body = (body, fields) => {
   return missing_fields;
 };
 
-module.exports = { _validate_email, _validate_body };
+const _response = (status, body) => {
+  res = {};
+  res.status = status;
+  res.body = body;
+
+  return res;
+};
+
+const _insert = (table, body) => {
+  const columns = Object.keys(body);
+  const values = Object.values(body).map((value) => `'${value}'`);
+
+  return (query = `INSERT INTO ${table} (${columns.join(
+    ', '
+  )}) VALUES (${values.join(', ')});`);
+};
+
+const _delete = (table, id) => {
+  return (query = `DELETE FROM ${table} WHERE id='${id}';`);
+};
+
+const _select = (table, id) => {
+  return (query = `SELECT * FROM ${table} WHERE id=${id};`);
+};
+
+const _update = (table, id, body) => {
+  const columns = Object.keys(body);
+  const values = Object.values(body);
+
+  const data = columns
+    .map((column, i) => {
+      return `${column}='${values[i]}'`;
+    })
+    .join();
+
+  return (query = `UPDATE ${table} SET ${data} WHERE id=${id};`);
+};
+
+module.exports = {
+  _validate_email,
+  _validate_body,
+  _response,
+  _insert,
+  _delete,
+  _select,
+  _update,
+};
