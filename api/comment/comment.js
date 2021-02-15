1cb0ce5ab75a1dd7ec49f70af949c06d4b145dba
miseongk <39994337+miseongk@users.noreply.github.com>
2021-02-15 22:16:36 +0900

    Add delete api

--- api/comment/comment.js
+++ api/comment/comment.js
@@ -3,11 +3,25 @@ const router = express.Router();
 
 const _query = require("../../database/db");
 const _auth = require("../user/auth");
+const utils = require("../../utils/utils");
 
 router.use((req, res, next) => {
     console.log(`${req.method}  ${req.ip} requested on ${req.path}`);
     next();
   });
+
+  router.get('/comments/:post_id', _auth, async(req,res) => {
+    let query_response = { status : "200 OK!" };
+
+    try{
+        query_response.data = await _query(`SELECT * FROM Comment WHERE post = ${req.params.post_id};`);
+    }
+    catch(error){
+        query_response.status = "400 Bad Request.";
+        query_response.data = error;
+    }
+    res.send(query_response);
+});
 
 router.post('/comments/:post_id', _auth, async(req,res) => {
     let query_response = { status : "200 OK!" };
@@ -57,7 +71,7 @@ router.post('/comments/:comment_id/reply', _auth, async(req, res) => {
     res.send(query_response);
 })
 
-router.get('/comments/:comment_id', _auth, async(req, res) => {
+router.delete('/comments/:comment_id', _auth, async(req,res) => {
     let query_response = { status : "200 OK!" };
 
     const writer = res.locals.user_id;
@@ -65,35 +79,19 @@ router.get('/comments/:comment_id', _auth, async(req, res) => {
     const post = await _query(`SELECT post FROM Comment WHERE id = ${comment_id};`);
     const post_writer = await _query(`SELECT writer FROM Post WHERE id = ${post[0].post};`);
     const comment_writer = await _query(`SELECT writer FROM Comment WHERE id = ${comment_id};`);
-    
+
     try{
         if(writer == post_writer[0].writer) {
-            query_response.message = "You can delete."
+            await _query(utils._delete("Comment", comment_id));
+            query_response.message = `You delete a comment #${comment_id} successfully.`;
         }
-        else if(writer == comment_writer) {
-            query_response.message = "You can delete."
+        else if(writer == comment_writer[0].writer) {
+            await _query(utils._delete("Comment", comment_id));
+            query_response.message = `You delete a comment #${comment_id} successfully.`;
         }
         else {
-            query_response.status = "400 Bad Request.";
-            query_response.message = "No authority."
+            query_response.message = "No authority.";
         }
-    }
-    catch(error){
-        query_response.status = "400 Bad Request.";
-        query_response.data = error;
-    }
-    res.send(query_response);
-})
-
-router.delete('/comments/:comment_id', _auth, async(req,res) => {
-    let query_response = { status : "200 OK!" };
-
-    const id = req.params.comment_id;
-
-    try{
-        await _query(
-            `DELETE FROM Comment WHERE id = ${id};`
-        );
     }
     catch(error){
         query_response.status = "400 Bad Request.";
