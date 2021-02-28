8a8e9f27b07e54ff5bf6cfe836a8bb0422fbe47e
jamesj0918 <jamesj0107@naver.com>
2021-02-28 14:13:30 +0900

    Add unfollow request API

--- api/user/user.js
+++ api/user/user.js
@@ -111,6 +111,11 @@ router.get("/users", _auth, async (req, res) => {
 router.put("/users/:user_id/follow", _auth, async (req, res) => {
   let query_response = {};
 
+  if (req.params.user_id == res.locals.user_id) {
+    query_response.message = "You can not follow yourself";
+    return res.send(query_response);
+  }
+
   try {
     let query = await _query(
       `SELECT user_id, name, is_private FROM User WHERE user_id='${req.params.user_id}'`
@@ -128,10 +133,63 @@ router.put("/users/:user_id/follow", _auth, async (req, res) => {
           await _query(
             `INSERT INTO User_User (target_user, request_user) VALUES ('${target_user.user_id}', '${res.locals.user_id}')`
           );
+          query_response.message = `Follow request has been sent to ${target_user.user_id}`;
         } else if (data[0].accepted === 0) {
+          res.status(400);
           query_response.message = `You have already requested to follow ${target_user.user_id}`;
         } else {
+          res.status(400);
           query_response.message = `You are already following ${target_user.user_id}`;
+        }
+      } catch (error) {
+        res.status(400);
+        query_response.message = error;
+      }
+    }
+  } catch (error) {
+    res.status(400);
+    query_response.message = error;
+  }
+
+  res.send(query_response);
+});
+
+// Unfollow request API
+router.put("/users/:user_id/unfollow", _auth, async (req, res) => {
+  let query_response = {};
+
+  if (req.params.user_id == res.locals.user_id) {
+    query_response.message = "You can not unfollow yourself";
+    return res.send(query_response);
+  }
+
+  try {
+    let query = await _query(
+      `SELECT user_id, name, is_private FROM User WHERE user_id='${req.params.user_id}'`
+    );
+    if (query.length === 0) {
+      res.status(400);
+      query_response.message = `User with user_id ${req.params.user_id} does not exists`;
+    } else {
+      let target_user = query[0];
+      try {
+        let follow_req = await _query(
+          `SELECT * FROM User_User WHERE target_user='${target_user.user_id}' AND request_user='${res.locals.user_id}'`
+        );
+        if (follow_req.length === 0) {
+          query_response.message = `You are not following user_id ${target_user.user_id}`;
+        } else {
+          if (follow_req[0].accepted === 0) {
+            query_response.message = `You have cancelled your follow request to user_id ${req.params.user_id}`;
+          } else {
+            query_response.message = `You have succesfully unfollowed user_id ${req.params.user_id}`;
+          }
+          try {
+            await _query(`DELETE FROM User_User WHERE id=${follow_req[0].id}`);
+          } catch (error) {
+            res.status(400);
+            query_response.message = error;
+          }
         }
       } catch (error) {
         res.status(400);
@@ -150,6 +208,11 @@ router.put("/users/:user_id/follow", _auth, async (req, res) => {
 router.put("/users/:user_id/accept", _auth, async (req, res) => {
   let query_response = {};
 
+  if (req.params.user_id == res.locals.user_id) {
+    query_response.message = "You can accpept yourself";
+    return res.send(query_response);
+  }
+
   let query = await _query(
     `SELECT user_id, name, is_private FROM User WHERE user_id='${req.params.user_id}'`
   );
@@ -165,7 +228,6 @@ router.put("/users/:user_id/accept", _auth, async (req, res) => {
         res.status(400);
         query_response.message = `You have no follow request from ${req.params.user_id}`;
       } else if (follow_req[0].accepted === 1) {
-        res.status(400);
         query_response.message = `You have already accepted the follow request from ${req.params.user_id}`;
       } else {
         try {
