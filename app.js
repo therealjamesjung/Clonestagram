6ee3ce6896f367f62dd7a7388252cae46de626de
miseongk <39994337+miseongk@users.noreply.github.com>
2021-03-15 01:42:28 +0900

    Add file upload api

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const API_ROOT = "/api/v1/";
 const user = require("./api/user/user");
 const post = require("./api/post/post");
 const comment = require("./api/comment/comment");
+const file = require("./api/file/file");
 
 app.use(cors());
 app.use(bodyparser.json());
@@ -18,6 +19,7 @@ app.use(bodyparser.urlencoded({ extended: true }));
 app.use(API_ROOT, user);
 app.use(API_ROOT, post);
 app.use(API_ROOT, comment);
+app.use(API_ROOT, file);
 
 app.listen(API_PORT, API_HOST, () => {
   console.log(`Clonestagram running at http://${API_HOST}:${API_PORT}`);
